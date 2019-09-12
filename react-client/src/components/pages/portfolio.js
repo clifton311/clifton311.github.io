da3dce41d8bc12124cfee042054c5e00562add44
clifton ho <cliftonho311@gmail.com>
2019-09-12 11:52:50 -0700

    Updated project descriptions

--- react-client/src/components/pages/portfolio.js
+++ react-client/src/components/pages/portfolio.js
@@ -6,13 +6,13 @@ import SDC from './photos/sdc.png';
 const portfolio = () => {
   return (
     <div>
-       <h3 className="portfolioHeader">Here are some of the fullstack projects I have been working on!</h3> <br/>
+       <h3 className="portfolioHeader">Here are some of the fullstack projects I have built:</h3> <br/>
         <div className="grid-container">
             <div className='grid-item-photo'>
               <img src={FEC} className="fec-image" alt='FEC'></img>
             </div>
             <p className="grid-item">
-              Movie reviews microservice designed with service oriented architecture. Back end was build with Express/Node and deployed to AWS EC2, load balanced with NGINX and cached responses with Redis
+              Movie reviews microservice designed with Service Oriented Architecture. Back end was build with Express/Node and deployed to AWS EC2, load balanced with NGINX and cached responses with Redis.
             <br></br>   
                   
               <div className="github">
@@ -25,7 +25,7 @@ const portfolio = () => {
               </div>
 
               <p className="grid-item">
-                A fullstack application renders housing information. Designed the rendering of housing features.
+                A fullstack application that renders housing information. Designed the rendering of housing features with React for the front end and Node for the backend. Used MySQL for the database.
                 <br/>
                 <div className="github">
                   <a href="https://github.com/code-voyage/zillowish-clifton" target="_blank" >Github</a>
@@ -36,10 +36,10 @@ const portfolio = () => {
               <img src={Recipe} className="fec-image" alt='FEC'></img> 
             </div>
             <p className="grid-item">
-              Full stack recipe finder application.
+              Full stack recipe finder application built with React, React-Router and incorporated OAuth via Passport.JS with a MySQL backend.
               <br />
               <div className="github">
-                <a href="https://github.com/team-goldwasser/Audience_Review_clifton" target="_blank" >Github</a>
+                <a href="https://github.com/clifton311/hrla-mvp-starter" target="_blank" >Github</a>
               </div>
             </p>
       </div>
