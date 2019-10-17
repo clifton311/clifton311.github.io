179ff4aaa700a62e55a3b3010cc38f2536e312fe
clifton ho <cliftonho311@gmail.com>
2019-10-17 13:06:38 -0700

    add express compression

--- server/index.js
+++ server/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
 const path = require('path');
+const compression = require("compression");
 
 const app = express();
+
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) return false;
+  return compression.filter(req, res);
+}
+
+app.use(compression({
+  level: 2,               
+  filter: shouldCompress, 
+}));
+
 app.use(express.static(`${__dirname}/../react-client/dist`));
 
 app.get('*', (req, res) => {
